f2486dfb6f82e0ca003f492f8b0c5c4647061018
Emilio-115 <emivazcru@alum.us.es>
2024-02-24 10:53:30 +0100

    Añadidos atributos restaurant

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -1,3 +1,5 @@
+const { INTEGER } = require('sequelize')
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Restaurants', {
@@ -6,8 +8,82 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
+      },
+      restaurantCategoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'RestaurantCategories'
+          },
+          key: 'id'
+        },
+        onDelete: 'cascade'
+      },
+
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'Users'
+          },
+          key: 'id'
+        },
+        onDelete: 'cascade'
+      },
+
+      url: {
+        type: Sequelize.STRING
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      postalCode: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      shippingCosts: {
+        allowNull: false,
+        type: Sequelize.DOUBLE,
+        defaultValue: 10
+      },
+      averageServiceMinutes: {
+        type: Sequelize.DOUBLE
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      logo: {
+        type: Sequelize.STRING
+      },
+      heroImage: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.ENUM(['online', 'offline', 'closed', 'temporarily closed'])
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
       }
-      // TODO: Include the rest of the fields of the Restaurants table
 
     })
   },
